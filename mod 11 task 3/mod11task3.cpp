d16c04ccbdab6d014bc03a75818897fc59660def
Evgeniy Korshunov <korshunov1980@rambler.ru>
2022-03-16 12:30:40 +0800

    mod11task3

---
+++ mod 11 task 3/mod11task3.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+std::string IP_Address_Validation_IPv4 (std::string str) {
+    bool coincidence = true;
+    std::string partStr;
+    int i = 0;
+    int counter = 0;
+
+    for (; i <= str.length(); i++) {
+        if (str[i] == '.') {
+            coincidence = false;
+            break;
+        }
+        int xPartStr = 0;
+        for (; i < str.length() && str[i] != '.'; i++) {
+            if (str[i] < '0' || str[i] > '9') {
+                coincidence = false;
+                break;
+            }
+            partStr += str[i];
+            xPartStr = (xPartStr + str[i] - '0') * 10;
+        }
+        xPartStr /= 10;
+
+        if (partStr.length() < 1 || xPartStr > 255 || partStr.length() == 2 && xPartStr < 10 ||
+                partStr.length() == 3 && xPartStr < 100) {
+            coincidence = false;
+            break;
+        }
+        partStr = "";
+        counter++;
+    }
+    return (coincidence && counter == 4 ? "Yes\n" : "No\n");
+}
+
+int main() {
+    std::cout << "Input an IP address to check (IPv4)\n";
+    std::string str;
+    getline(std::cin, str);
+
+    std::cout << IP_Address_Validation_IPv4 (str);
+
+    return 0;
+}
