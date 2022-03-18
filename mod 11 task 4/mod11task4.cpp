20ced49de5c38e37bcd4bf017576ca85f7eea24a
Evgeniy Korshunov <korshunov1980@rambler.ru>
2022-03-18 14:36:44 +0800

    correct mod11task4

--- mod 11 task 4/mod11task4.cpp
+++ mod 11 task 4/mod11task4.cpp
@@ -2,27 +2,18 @@
 
 std::string Integer (std::string strNumber) {
     std::string integer;
-    std::string fractionalNumber;
     int i = 0;
-    for (; i < strNumber.length();) {
-        for (; i < strNumber.length() && strNumber[i] != '.'; i++) {
-            integer += strNumber[i];
-        }
-        i++;
-        for (;i < strNumber.length(); i++) {
-            fractionalNumber += strNumber[i];
-        }
+    while (strNumber[i] == '0') i++;
+    for (; i < strNumber.length() && strNumber[i] != '.'; i++) {
+        integer += strNumber[i];
     }
     return integer;
 }
 std::string FractionalNumber (std::string strNumber) {
-    std::string integer;
     std::string fractionalNumber;
     int i = 0;
     for (; i < strNumber.length();) {
-        for (; i < strNumber.length() && strNumber[i] != '.'; i++) {
-            integer += strNumber[i];
-        }
+        for (; i < strNumber.length() && strNumber[i] != '.'; i++);
         i++;
         for (;i < strNumber.length(); i++) {
             fractionalNumber += strNumber[i];
@@ -81,55 +72,33 @@ int main() {
     std::string fractionalNumber1 = FractionalNumber(strNumber1);
     std::string fractionalNumber2 = FractionalNumber(strNumber2);
 
-    int i1 = 0;
-    int i2 = 0;
-    int x1 = 0;
-    int x2 = 0;
-    bool checkbox1 = true;
-    bool checkbox2 = true;
+    for (int i = 0 ;i < integer1.length() && i < integer2.length(); ++i) {
 
-    while (i1 < integer1.length() || i2 < integer2.length()) {
-        while (integer1[i1] == '0' && checkbox1) {
-            i1++;
-            x1++;
-        }
-        checkbox1 = false;
-
-        while (integer2[i2] == '0' && checkbox2) {
-            i2++;
-            x2++;
-        }
-        checkbox2 = false;
-
-        if (integer1[0] == '-' && integer2[0] != '-' || ((integer1[i1] == integer1[integer1.length() - 1 - x1])
-                                                         && integer1.length() < integer2.length()) ||
-            (integer1.length() == integer2.length() && integer1[i1] < integer2[i2])) {
+        if (integer1[0] == '-' && integer2[0] != '-' || integer1.length() < integer2.length() || (integer1.length() == integer2.length() && integer1[i] < integer2[i])) {
             std::cout << "Less\n";
             return 0;
         }
-        if (integer2[0] == '-' && integer1[0] != '-' || ((integer2[i2] == integer2[strNumber2.length() - 1 - x2])
-                                                         && integer2.length() < integer1.length()) ||
-            (integer2.length() == integer1.length() && integer2[i2] < integer1[i1])) {
+        if (integer2[0] == '-' && integer1[0] != '-' || integer2.length() < integer1.length() || (integer2.length() == integer1.length() && integer2[i] < integer1[i])) {
             std::cout << "More\n";
             return 0;
         }
-        i1++;
-        i2++;
     }
 
-    if (fractionalNumber1 == "" && fractionalNumber2 == "") {
+    if (fractionalNumber1 == fractionalNumber2) {
         std::cout << "Equal\n";
         return 0;
     }
 
+    while (fractionalNumber1.length() < fractionalNumber2.length()) fractionalNumber1 += '0';
+    while (fractionalNumber2.length() < fractionalNumber1.length()) fractionalNumber2 += '0';
+
     for (int i = 0; i < fractionalNumber1.length() && i < fractionalNumber2.length(); i++) {
-        if (fractionalNumber1[0] == '-' && fractionalNumber2[0] != '-' || ((fractionalNumber1[i] == fractionalNumber1[fractionalNumber1.length()-1])
-                && fractionalNumber1.length() < fractionalNumber2.length()) || fractionalNumber1[i] < fractionalNumber2[i]) {
+
+        if (fractionalNumber1[i] < fractionalNumber2[i]) {
             std::cout << "Less\n";
             return 0;
         }
-        if (fractionalNumber2[0] == '-' && fractionalNumber1[0] != '-' || ((fractionalNumber2[i] == fractionalNumber2[fractionalNumber2.length()-1])
-                && fractionalNumber2.length() < fractionalNumber1.length()) || fractionalNumber2[i] < fractionalNumber1[i]) {
+        if (fractionalNumber2[i] < fractionalNumber1[i]) {
             std::cout << "More\n";
             return 0;
         }
