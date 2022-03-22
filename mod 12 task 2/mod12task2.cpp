03b1a5d295cce08ffc3eda60b52fc5e5271c4098
Evgeniy Korshunov <korshunov1980@rambler.ru>
2022-03-23 01:17:51 +0800

    corrected2 mod12task2

--- mod 12 task 2/mod12task2.cpp
+++ mod 12 task 2/mod12task2.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 
 int main() {
-    int numbers[] = {114,111, 106, 107, 108, 105, 115,  117, 110, 109, 112, 113, 116, 117, 118};
+    int numbers[15] = {105, 114, 111, 106, 107, 108, 115, 108, 110, 109, 112, 113, 116, 117, 118};
+
+    int n = 15;
+    int min = numbers[0];
+    int sum = 0;
+
+    for(int i = 0; i < n; ++i)
+    {
+        if(min > numbers[i])
+            min = numbers[i];
+
+        sum += numbers[i];
+    }
+
+    for(int i = min; i < min + n - 1; ++i)
+        sum -= i;
+
+    std::cout << sum;
+    return 0;
+}
+
+    /*int numbers[] = {114,111, 106, 107, 108, 105, 115,  117, 110, 109, 112, 113, 116, 117, 118};
     int min = 0;
     for (int i = 0; i < 15; i++) {
         for (int k = i; k > 0 ; k--) {
@@ -16,4 +37,4 @@ int main() {
             }
         }
     }
-}
+}*/
