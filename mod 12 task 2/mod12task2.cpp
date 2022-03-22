ef78ee94dbe9bc7dece9890a1018284ab850a418
Evgeniy Korshunov <korshunov1980@rambler.ru>
2022-03-23 00:03:05 +0800

    corrected mod12task2

--- mod 12 task 2/mod12task2.cpp
+++ mod 12 task 2/mod12task2.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 
 int main() {
-    int numbers[] = {114,111, 106, 107, 108, 105, 115,  108, 110, 109, 112, 113, 116, 117, 118};
-    int startNumber = 105;
+    int numbers[] = {114,111, 106, 107, 108, 105, 115,  117, 110, 109, 112, 113, 116, 117, 118};
+    int min = 0;
+    for (int i = 0; i < 15; i++) {
+        for (int k = i; k > 0 ; k--) {
+            if (numbers[k] == numbers[k - 1]) {
+                std::cout << numbers[k] << "\n";
+                return 0;
+            }
+            if (numbers[k] < numbers[k - 1]) {
+                min = numbers[k];
+                numbers[k] = numbers[k - 1];
+                numbers[k - 1] = min;
+            }
+        }
+    }
+
+
+
+    /*int startNumber = 105;
     startNumber --;
     int storageNumber1;
     int storageNumber2 = startNumber;
@@ -20,5 +37,5 @@ int main() {
             std::cout << storageNumber1 << "\n";
             break;
         }
-    }
+    }*/
 }
