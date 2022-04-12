868cd7dd61ba5bb2a11a6239c9048e4dbd012fb5
Evgeniy Korshunov <korshunov1980@rambler.ru>
2022-04-13 00:15:07 +0800

    13.1

--- mod 13 task 1/main.cpp
+++ mod 13 task 1/main.cpp
@@ -17,15 +17,26 @@ int main() {
     std::cout << "What value to remove from the vector? \n";
     std::cin >> number;
     int c = 0;
-    for (int i = 0; i < amount; ++i) {
-        while (vec[i+c] == number) {
-            ++c;
-            --amount;
-        }
-        vec[i] = vec[i + c];
+    for (int i = 0; i < vec.size(); ++i) {
+        if (vec[i] != number)
+            vec[c++] = vec[i];
     }
-    vec.resize(amount);
+    vec.resize(c);
     if (vec.empty()) std::cout << "No numbers left!!!\n";
     for (int i = 0; i < vec.size(); ++i)
         std::cout << vec[i] << " ";
 }
+
+/*int main() {
+    std::vector<int> v = {1,2,3,3,5,3,5,7,3};
+    int value = 3;
+
+    int j = 0;
+    for (int i = 0; i < v.size(); ++i) {
+        if (v[i] != value)
+            v[j++] = v[i];
+    }
+    v.resize(j);
+
+    for (int i = 0; i < v.size(); ++i) std::cout<<v[i]<<std::endl;
+}*/
