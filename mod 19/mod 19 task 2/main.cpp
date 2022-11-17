53d27740b90d6c0fce4c2a972cab25d9575e34c6
Evgeniy Korshunov book <korshunovea1980@gmail.com>
2022-11-17 14:19:54 +0800

    mod 19 task 2 1

--- mod 19/mod 19 task 2/main.cpp
+++ mod 19/mod 19 task 2/main.cpp
@@ -1,41 +1,28 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 int main() {
     std::ifstream words;
     std::string directory;
     char buffer[50];
     std::cout << "Input valid directory\n";
-    //std::cin >> directory;
-    directory = "C:\\Users\\korsh\\skillbox-project\\words2.txt";
+    getline(std::cin, directory);
+    //directory = "C:\\Users\\korsh\\skillbox-project\\mod 19\\mod 19 task 2\\words.txt";
     words.open(directory, std::ios::binary);
-    //bool loginVerification = words.is_open();
     while (!(words.is_open())) {
-        //words.close();
         std::cout << "ERROR!!! Input valid directory\n";
-        std::cin >> directory;
+        getline(std::cin, directory);
         words.open(directory, std::ios::binary);
     }
-    while (!words.eof()){
-        //std::cout << words.tellg() << "!!!!!!!!!!!!!!!!!!!!!";
+     while (!words.eof()){
         words.read(buffer, sizeof(buffer));
-        //words.seekg(0);
         if (words.eof()) {
-            for (int i = 0; i < 34; ++i) {
+            for (int i = 0; i < words.gcount(); ++i) {
                 std::cout << buffer[i];
             }
-            //words.seekg(0);
-            //buffer[30];
-
-            //words.read(buffer, sizeof(buffer));
-            break;
-        }
-        std::cout << buffer;
+        } else std::cout << buffer;
     }
-    std::cout << "\n\n\n" << buffer;
-    /*for (int i = 0; i < 10; ++i) {
-        std::cout << buffer[i];
-    }*/
     words.close();
     return 0;
 }
